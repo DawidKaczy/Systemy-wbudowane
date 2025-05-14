86c3f4206beef0233bc82442e1b9ffb52bb45aab
DawidKaczy <116282752+DawidKaczy@users.noreply.github.com>
2025-05-14 12:50:17 +0200

    Update and rename newmainXC16.c to zad2.c

--- zad2.c
+++ zad2.c
@@ -52,7 +52,7 @@ int main(void) {
     while(1) {
         value_poten = ADC_Read10bit(ADC_CHANNEL_POTENTIOMETER);
 
-        ok = value_poten / 205; //1024 / 5 = 204.8  zaokr?glamy w dó? do 205, ?eby równo podzieli? zakres.
+        ok = value_poten / 205; //1024 / 5 = 204.8  zaokr?glamy w dÃ³? do 205, ?eby rÃ³wno podzieli? zakres.
         switch(ok) {
             case 0: opoznienie = 100000;    break;
             case 1: opoznienie = 300000;    break;
